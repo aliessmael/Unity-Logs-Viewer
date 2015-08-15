1121aa2f7b23b406731ea478d19f8664d9ead1c3
fog <blue_fog@163.com>
2015-08-15 17:03:30 +0800

    1. Add namespace LogViewer. 2. Add key binding in unity editor. 3. Add a default Reporter prefab . 4. Remove core code to Core directory.

--- Reporter/Test/Rotate.cs
+++ Reporter/Test/Rotate.cs
@@ -1,17 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Rotate : MonoBehaviour {
+namespace LogsViewer
+{
+	public class Rotate : MonoBehaviour
+	{
 
-	Vector3 angle ;
-	// Use this for initialization
-	void Start () {
-		angle = transform.eulerAngles ;
-	}
+		Vector3 angle;
+		// Use this for initialization
+		void Start ()
+		{
+			angle = transform.eulerAngles;
+		}
 	
-	// Update is called once per frame
-	void Update () {
-		angle.y += Time.deltaTime * 100 ;
-		transform.eulerAngles = angle ;
+		// Update is called once per frame
+		void Update ()
+		{
+			angle.y += Time.deltaTime * 100;
+			transform.eulerAngles = angle;
+		}
 	}
 }
